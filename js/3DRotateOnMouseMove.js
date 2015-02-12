e1f3d1e57faa0799090dd002f9bf39d2db32510c
kinpa200296 <kinpa200296@yandex.ru>
2015-02-12 23:25:58 +0300

    Added Menu Bar Example Added 3D Cube Example

---
+++ js/3DRotateOnMouseMove.js
@@ -0,0 +1,51 @@
+var MouseMoveRotatingElements = new Array();
+
+function Attach3DRotationById(id) {
+    var elem = document.getElementById(id);
+    if (elem !== null) {
+        MouseMoveRotatingElements.push(elem);
+    }
+}
+
+function Attach3DRotationByClassName(className){
+    var newElements = document.getElementsByClassName(className);
+    for (var i = 0; i < newElements.length; i++)
+    {
+        var elem = newElements.item(i);
+        if (elem !== null) {
+            MouseMoveRotatingElements.push(elem);
+        }
+    }
+}
+
+function RotateAllElementsOnMouseMove(params) {
+    for (var i = 0; i < MouseMoveRotatingElements.length; i++)
+    {
+        var elem = MouseMoveRotatingElements[i];
+        elem.style.mozTransform='rotateY('+params.pageY+'deg) rotateX('+params.pageX+'deg)';
+        elem.style.webkitTransform='rotateY('+params.pageY+'deg) rotateX('+params.pageX+'deg)';
+        elem.style.transform='rotateY('+params.pageY+'deg) rotateX('+params.pageX+'deg)';
+
+    }
+}
+
+function RotateElement(params){
+    var elem = params.toElement;
+    if (elem !== null) {
+        elem.style.mozTransform = 'rotateY(' + params.pageY + 'deg) rotateX(' + params.pageX + 'deg)';
+        elem.style.webkitTransform = 'rotateY(' + params.pageY + 'deg) rotateX(' + params.pageX + 'deg)';
+        elem.style.transform = 'rotateY(' + params.pageY + 'deg) rotateX(' + params.pageX + 'deg)';
+    }
+}
+
+function RotateOnWindowMouseMove(){
+    window.onmousemove = RotateAllElementsOnMouseMove;
+}
+
+function RotateOnElementMouseMove(){
+    for (var i = 0; i < MouseMoveRotatingElements.length; i++)
+    {
+        var elem = MouseMoveRotatingElements[i];
+        elem.onmousemove = RotateElement;
+    }
+}
